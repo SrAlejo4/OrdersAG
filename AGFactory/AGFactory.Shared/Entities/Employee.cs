eb30348438298ba2017df38f4c2797c4d7032897
SrAlejo4 <alejandrogarci087@gmail.com>
2025-09-13 08:30:07 -0500

    Creating Employee entity and DataBase

---
+++ AGFactory/AGFactory.Shared/Entities/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AGFactory.Shared.Entities;
+
+public class Employee
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Primer nombre")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio")]
+    [MaxLength(30, ErrorMessage = "El campo {0} no puede tener más de {1}" + "carácteres.")]
+    public string FirstName { get; set; } = null!;
+
+    [Display(Name = "Segundo Nombre")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio")]
+    [MaxLength(30, ErrorMessage = "El campo {0} no puede tener más de {1}" + "carácteres.")]
+    public string LastName { get; set; } = null!;
+
+    [Display(Name = "Activo")]
+    public bool IsActive { get; set; }
+
+    [Display(Name = "Fecha contratación")]
+    public DateTime HireDate { get; set; }
+
+    [Display(Name = "Salario")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio")]
+    [Range(1000000, double.MaxValue, ErrorMessage = "El salario debe ser mínimo de $1.000.000.")]
+    public decimal Salary { get; set; }
+}
