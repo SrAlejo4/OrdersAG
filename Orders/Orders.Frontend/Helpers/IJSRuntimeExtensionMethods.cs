618465ccf438080132ee1f8d5035ed1c1237264c
SrAlejo4 <alejandrogarci087@gmail.com>
2025-11-27 14:22:34 -0500

    Add Auth links

--- Orders/Orders.Frontend/Helpers/IJSRuntimeExtensionMethods.cs
+++ Orders/Orders.Frontend/Helpers/IJSRuntimeExtensionMethods.cs
@@ -1,5 +1,21 @@
-﻿namespace Orders.Frontend.Helpers;
+﻿using Microsoft.JSInterop;
 
-public interface IJSRuntimeExtensionMethods
+namespace Orders.Frontend.Helpers;
+
+public static class IJSRuntimeExtensionMethods
 {
+    public static ValueTask<object> SetLocalStorage(this IJSRuntime js, string key, string content)
+    {
+        return js.InvokeAsync<object>("localStorage.setItem", key, content);
+    }
+
+    public static ValueTask<object> GetLocalStorage(this IJSRuntime js, string key)
+    {
+        return js.InvokeAsync<object>("localStorage.getItem", key);
+    }
+
+    public static ValueTask<object> RemoveLocalStorage(this IJSRuntime js, string key)
+    {
+        return js.InvokeAsync<object>("localStorage.removeItem", key);
+    }
 }
